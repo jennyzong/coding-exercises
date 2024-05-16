f5a1da7030626244e29f3a62a801105c0343070f
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-16 11:18:53 -0400

    exercises from chrome extension
    -truthy and falsy value

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-// reference html elements
-const form = document.getElementById('form');
-const numOneInput = document.getElementById('num1');
-const numTwoInput = document.getElementById('num2');
-const resultText = document.getElementById('result');
-
-// form event listener
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    // we can convert the strings into numbers using the Number() function
-    const numOneVal = Number(numOneInput.value); 
-    const numTwoVal = Number(numTwoInput.value);
-    
-    console.log(`numOneVal is of type: ${typeof numOneVal}, with a value of ${numOneVal}`);
-    console.log(`numTwoVal is of type: ${typeof numTwoVal}, with a value of ${numTwoVal}`);
-    // now the numbers add up correctly
-    resultText.innerText = `${numOneVal} + ${numTwoVal} = ${numOneVal + numTwoVal}`;
-})
+console.log(  Boolean("")   ) // fale
+console.log(  Boolean("0")  ) // true
+console.log(  Boolean(100)  ) // true
+console.log(  Boolean(null) ) // false
+console.log(  Boolean([0])  ) // true
+console.log(  Boolean(-0)   ) // false
