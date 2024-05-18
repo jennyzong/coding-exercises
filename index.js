c933d34e0f5cb2a97edb1a9706bfaca337868876
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-17 21:07:28 -0400

    exercise from chrome extension
    - arguments VS parameters

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-// Create a function, add(), that adds two numbers together and returns the sum
+// What are greeting and name? parameters
+// What are "Howdy" and "James"? arguments
+// What are num1 and num2? parameters
+// What are 3 and 4?  arguments
+
+
+//.                parameters
+function greetUser(greeting, name) { 
+    welcomeEl.textContent = `${greeting}, ${name} 👋`
+}
+
+//.        arguments
+let hi = "Howdy"
+greetUser(hi, "James")
+
 
 function add(num1, num2) {
     return num1 + num2
 }
 
-console.log( add(3, 4)   ) // should log 7
-console.log( add(9, 102) ) // should log 111
+add(3, 4)
