6a413c59a2b5e4c3b77922a4cdb072a876ddd6b9
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-20 21:23:24 -0400

    exercise from cookie consent
    -forms for Astronaut email and password

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/*
-Challenge:
-1. Take control of the 'modal' div with JavaScript.
-2. Swap out our console.log for a line of code
-   which will change the CSS 'display' property
-   of our modal to 'inline'.
-*/
 
-setTimeout(function(){
-    console.log('Modal Opened!')
-}, 1500)
+
+
