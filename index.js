6a1c50cc9ec6708e612bd638979aa15c1632c0f4
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-20 20:49:03 -0400

    exercise from cookie consent
    -element.style

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-/*
-Challenge:
-1. Log out the phrase "Modal Opened!" 
-   after a 1.5 second delay.
-*/
+const revealBtn = document.getElementById('reveal-btn')
+const answer = document.getElementById('answer')
+const question = document.getElementById('question')
 
-setTimeout( function(){
-    console.log("Modal Opened!" )
-}, 1500)
+revealBtn.addEventListener('click', function(){
+    answer.style.display = 'block'
+    question.style.color = '#1434A4'
+    question.style.backgroundColor = '#68e1fd'
+})
+
+/*
+Challenge
+1. When the button is clicked and the answer revealed, 
+   change the background color to #68e1fd and text color
+   to #1434A4 just in the "question" div.
+   ⚠️ You will run into a problem with the background 
+   color. See if you can find the solution by googling.
+2. When the answer is revealed, make the button disappear.
+*/
