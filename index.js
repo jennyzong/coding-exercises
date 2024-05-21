deeaa12b51c43e4a8c9f453dcb282b6c6c41ac87
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-20 20:55:33 -0400

    exercise from cookie consent
    -make the modal reappear

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-const revealBtn = document.getElementById('reveal-btn')
-const answer = document.getElementById('answer')
-const question = document.getElementById('question')
-
-revealBtn.addEventListener('click', function(){
-    answer.style.display = 'block'
-    question.style.color = '#1434A4'
-    question.style.backgroundColor = '#68e1fd'
-})
-
 /*
-Challenge
-1. When the button is clicked and the answer revealed, 
-   change the background color to #68e1fd and text color
-   to #1434A4 just in the "question" div.
-   ⚠️ You will run into a problem with the background 
-   color. See if you can find the solution by googling.
-2. When the answer is revealed, make the button disappear.
+Challenge:
+1. Take control of the 'modal' div with JavaScript.
+2. Swap out our console.log for a line of code
+   which will change the CSS 'display' property
+   of our modal to 'inline'.
 */
+
+setTimeout(function(){
+    console.log('Modal Opened!')
+}, 1500)
