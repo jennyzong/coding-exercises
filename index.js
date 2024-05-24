3163ee41d30faee6e954e9341b89bd9db28c6c41
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-24 11:36:10 -0400

    exercise from cookie consent
    -Make the Sort by Price button reverse the order of the paintings.

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-const decrement = document.getElementById('decrement')
-const increment = document.getElementById('increment')
-const quantityDisplay = document.getElementById('quantity-display')
-const cartBtn = document.getElementById('cartBtn')
-
-let quantity = 0
-
-decrement.addEventListener('click', function(){
-    quantity--
-    if (quantity === 0){
-        decrement.disabled = true
-    }     
-    quantityDisplay.innerText = quantity
-})
-
-increment.addEventListener('click', function(){
-    quantity ++
-    decrement.disabled = false
-    quantityDisplay.innerText = quantity
-})
- 
-cartBtn.addEventListener('click', function(){
-    console.log(`Your order for ${quantity} pairs of shoes is being processed`)
+const sortBtn = document.getElementById('sort-btn')
+const container = document.getElementById('container')
+            
+sortBtn.addEventListener('click', function(){
+    
+    container.classList.toggle('reverse')
 /*
 Challenge:
-1. Disable the 'Add to Cart' button when 
-   quantity is at zero. Remember: quantity
-   will be at zero when the page loads!
-*/ 
+1. Make the Sort by Price button 
+   reverse the order of the paintings.
+*/
+
 })
-
-
