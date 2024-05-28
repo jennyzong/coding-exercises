9b0e8be40afe952762692a6991991858fc13d326
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-28 16:33:00 -0400

    exercise from pumpkin's meme picker
    -Remove the "unread" class from the parentElement of the clicked element.

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-// const emojis = ['🐥','🐯','🐼']
-// console.log(emojis.includes('🐴'))
-
-const addItemBtn = document.getElementById('add-item-btn')
-const itemInput = document.getElementById('item-input')
-const list = document.getElementById('list')
-
-const shoppingList = []
-    
-addItemBtn.addEventListener('click', function(){
+document.addEventListener('click', function(e){
 /*
 Challenge:
-1. Add an if else to the event listener's function.
-2. Only add an item to the shoppingList array if it 
-   is not already in the shoppingList array.
-3. If an item is a duplicate, clear the input field
-   and log out "no duplicates".
+1. Remove the "unread" class from the 
+   parentElement of the clicked element.
 */
-        if(shoppingList.includes(itemInput.value)){
-            console.log('no duplicates')
-        }
-        else{
-            shoppingList.push(itemInput.value)
-            render()        
-        }
-        itemInput.value = '' 
+    document.getElementById(e.target.id).parentElement.classList.add('read')
+    document.getElementById(e.target.id).parentElement.classList.remove("unread")
 })
-
-function render(){    
-    let html = ''
-    for (let item of shoppingList){
-        html+= `<li class="list-item">${item}</li>`
-    }
-    list.innerHTML = html
-}
-
-render()
-
