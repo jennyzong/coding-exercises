3b1ff135f48868c9faa943b8ec3eb59b5a4b5782
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-30 10:32:27 -0400

    querySelector
    -use querySelector log out the value of the checked radio option.

--- index.js
+++ index.js
@@ -1,68 +1,14 @@
-const container = document.getElementById('container')
-const clearBtn = document.getElementById('clear-btn')
+// We can much more easily target pseudo elements like :checked
+const submitBtn = document.getElementById('submit-btn')
 
-const products = [
-    {
-        name: 'Ostrich Pillow',
-        price: '10',
-        image: 'ostrichpillow.jpg',
-        id: 'ostrich-pillow'
-    },
-    {
-        name: 'Bacon Bandages',
-        price: '8',
-        image: 'bacon-bandage.jpg',
-        id: 'bacon-bandages'
-    },
-    {
-        name: 'Baby Mop',
-        price: '20',
-        image: 'babymop.jpg',
-        id: 'baby-mop'
-    }
-]
-
-for (let product of products){
-    container.innerHTML += `
-    <div class="product on-offer">
-        <h3>${product.name}</h3>
-        <h4> £${product.price}</h4>
-        <img src="${product.image}">
-        <button id="${product.id}">Buy Now</button>
-    </div>
-    `
-}
-
-container.addEventListener('click', function(e){
-    document.getElementById(e.target.id).parentElement.classList.add('purchased')
-    document.getElementById(e.target.id).parentElement.classList.remove('on-offer')
+submitBtn.addEventListener('click', function(){
+    const checkedRadio = document.querySelector('input[type="radio"]:checked')
+    console.log(checkedRadio.value)
 })
 
-clearBtn.addEventListener('click', function(){
 /*
 Challenge:
-1. Finish setting up this array of 
-   elements with the "product" class.
-2. Iterate over this array and remove 
-   the "purchase" class and add the 
-   "on-offer" class.
+1. Muscle memory exercise: log out 
+   the value of the checked radio 
+   option.
 */
-    const productsArray = document.getElementsByClassName('product')
-    for (let product of productsArray){
-        product.classList.remove('purchased')
-        product.classList.add('on-offer')
-        
-    }
-})
-
-
-
-
-
-
-
-
-
-
-
-
