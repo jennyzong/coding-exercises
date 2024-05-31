e80c3063ec910320f8d95d7bf680fc5d2cee133e
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-31 10:17:33 -0400

    filter()
    - Use the .filter method to create an array called "children" which holds all of the ages that are under 18.

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const continueBtn = document.getElementById('continue-btn')
-const acceptTerms = document.getElementById('accept-terms')
-const mailTerm = document.getElementById("mail-term")
+const ages = [1, 5, 9, 23, 56, 10, 47, 70, 10, 19, 23, 18]
 
-continueBtn.addEventListener('click', function(){
-    
+const adults = ages.filter(function(age){
+    return age >= 18
+})
+
+console.log(adults)
+
 /*
 Challenge:
-1. Add a second checkbox to see if a user
-   wants to be added to the mailing list.
-2. If they do, log out "mail accepted" when 
-   the continue button is clicked. If they
-   don't, log out "mail not accapted."
+1. Use the .filter method to create an array 
+   called "children" which holds all of the ages 
+   that are under 18.  
 */
-    if (acceptTerms.checked){
-        console.log("Terms accepted!")    
-    }
-    else{
-        console.log("Terms refused!")         
-    }
-    if(mailTerm.checked){
-        console.log("mail accepted")
-    }else{
-        console.log("mail not accapted")
-    }
+
+const children = ages.filter(function(age){
+    return age < 18 
 })
+
+console.log(children)
