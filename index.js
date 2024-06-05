21c351c751c61ee6cc2e17cd2ba96e9dd9a997e2
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-06-04 21:51:54 -0400

    Aside: forEach()
    -Nest a forEach to log out each individual power in each characters powers array.

--- index.js
+++ index.js
@@ -1,49 +1,31 @@
-const series = [
+const characters = [
     {
-        name: 'The Wire',
-        location: 'Baltimore',
-        lengthInHours : 60,
-        genres: ['action', 'thriller', 'detective', 'suspense']
+        title: 'Ninja',
+        emoji: '🥷',
+        powers: ['agility', 'stealth', 'aggression'],
     },
     {
-        name: 'Game of Thromes',
-        location: 'Westeros and Essos',
-        lengthInHours : 70.25,
-        genres: ['fantasy', 'action', 'tragedy']
+        title: 'Sorcerer',
+        emoji: '🧙',
+        powers: ['magic', 'invisibility', 'necromancy'],
     },
-    {
-        name: 'Friends',
-        location: 'New York',
-        lengthInHours : 85,
-        genres: ['comedy', 'romance', 'drama']
-    },
-    {
-        name: 'The Walking Dead',
-        location: 'Atlanta',
-        lengthInHours : 131,
-        genres: ['zombie', 'apocalypse', 'thriller', 'suspense']
-    },
-    {
-        name: 'The Big Bang Theory',
-        location: 'Pasadena',
-        lengthInHours : 139.66,
-        genres: ['comedy', 'nerd', 'romance']
-    },
+    { 
+        title: 'Ogre',
+        emoji: '👹',
+        powers: ['power', 'stamina', 'shapeshifting'],
+    },  
+    { 
+        title: 'Unicorn',
+        emoji: '🦄',
+        powers: [ 'flight', 'power', 'purity'],
+    }
 ]
 
-const newYorkSeries = series.filter(function(show){
-    return show.location === 'New York'
+// for (let character of characters){
+//     console.log(character)
+// }
+
+characters.forEach(function(character, index){
+    console.log(index, character.title)
 })
 
-const thrillers = series.filter(function(show){
-    return show.genres.includes('thriller')
-})
-
-console.log(thrillers)
-
-/*
-Challenge:
-1. Use the .filter() method to create an array
-   of all of the thrillers.
-   .includes('romance')
-*/
