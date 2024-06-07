06b41970e08adc8115fc7a7d5fe736b0962f305b
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-06-06 20:54:44 -0400

    Aside: Data Attr JS and Challenge
    -Make clicking on the heart icon log out the id of the image.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
- 
+document.addEventListener('click', function(e) {
+    if (e.target.dataset.share){
+        console.log(e.target.dataset.share)
+    }
+    else if (e.target.dataset.heart){
+        console.log(e.target.dataset.heart)
+    }
+/*
+Challenge:
+2. Make clicking on the heart icon log out
+   the id of the image.
+*/
+})
+
+
+
