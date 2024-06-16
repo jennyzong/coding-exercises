af291272e7276799aade0dbff838178243830af0
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-06-15 21:04:55 -0400

    Aside: Conditionally render CSS class
    - Add functionality so that when a user clicks the share icon it turns green.

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-document.addEventListener('click', function(e) {
-    if (e.target.dataset.share){
-        console.log(e.target.dataset.share)
-    }
-    else if (e.target.dataset.heart){
-        console.log(e.target.dataset.heart)
-    }
+const galleryContainer = document.getElementById('gallery-container')
+
 /*
 Challenge:
-2. Make clicking on the heart icon log out
-   the id of the image.
+1. Add functionality so that when a user 
+   clicks the share icon it turns green. 
+   Clicking it a second time should return 
+   it to white.
 */
+
+
+let isLiked = false
+
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.heart){
+        isLiked = !isLiked
+        render()
+    }
+    else if(e.target.dataset.share){
+        
+    }
 })
 
+function render(){ 
+    
+    let heartClass = ''
+    
+    if(isLiked){
+        heartClass = 'liked'
+    }
+       
+    let imageHtml = `
+    		<div id="image-1" class="img-container">
+			<img src="dino2.jpeg" alt="Man in front of dinosaur">
+			<div class="social-icons-container">
+				<i class="fa-solid fa-heart ${heartClass}" data-heart="image-1"></i>
+				<i class="fa-solid fa-share " data-share="image-1"></i>
+			</div>
+    `
+    galleryContainer.innerHTML = imageHtml
+}
 
-
+render()
