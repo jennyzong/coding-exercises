70b456b9652169b380eefe99454b92a161483af4
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-06-18 11:23:38 -0400

    Object Destructuring Challenge
    -Destructure the object and use the individual variables to log out one or more sentences about your dream holiday.

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
-const galleryContainer = document.getElementById('gallery-container')
-
-/*
-Challenge:
-1. Add functionality so that when a user 
-   clicks the share icon it turns green. 
-   Clicking it a second time should return 
-   it to white.
-*/
-
-
-let isLiked = false
-
-
-document.addEventListener('click', function(e){
-    if(e.target.dataset.heart){
-        isLiked = !isLiked
-        render()
-    }
-    else if(e.target.dataset.share){
-        
-    }
-})
-
-function render(){ 
-    
-    let heartClass = ''
-    
-    if(isLiked){
-        heartClass = 'liked'
-    }
-       
-    let imageHtml = `
-    		<div id="image-1" class="img-container">
-			<img src="dino2.jpeg" alt="Man in front of dinosaur">
-			<div class="social-icons-container">
-				<i class="fa-solid fa-heart ${heartClass}" data-heart="image-1"></i>
-				<i class="fa-solid fa-share " data-share="image-1"></i>
-			</div>
-    `
-    galleryContainer.innerHTML = imageHtml
+const dreamHoliday = {
+    destination: '',
+    activity: '',
+    accommodation: '',
+    companion: ''
 }
 
-render()
+/* 
+Challenge
+1. Complete the object dreamHoliday with whatever
+   information is true for you. Feel free to add 
+   extra properties or change the existing ones. 
+2. Destructure the object and use the individual 
+   variables to log out one or more sentences about 
+   your dream holiday. 
+   
+E.g. "I would love to go to Austin, Texas to visit the Tesla HQ. 
+     I'd sleep in a luxury ranch and hang out with Elon Musk all day."
+*/
