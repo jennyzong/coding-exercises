b7f696ea928caf0bc2717472c716362e38717de2
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-06-19 20:01:41 -0400

    The .join() Method Challenge
    -Use the .join() method to remove those commas!

--- index.js
+++ index.js
@@ -2,24 +2,26 @@ import { playlistArr } from '/playlist.js'
 
 /*
 Challenge
-1. Refactor the code below to use .map() 
-   instead of the for loop.
-   ⚠️ Don't worry about the commas for now.
+1. Use the .join() method to remove 
+   those annoying commas!
+⚠️ You will need to chain two methods
+   together to complete the challenge.
 */
 
 const playlistHtml = playlistArr.map(function(track){
-    return `<section class="card">
-    <div class="card-start">
-        <img src="/images/${track.albumArt}">
-    </div>
-        <div class="card-mid">
-            <h4 class="card-title">${track.title}</h4>
-            <p class="card-artist">${track.artist}</p>
+    return `
+    <section class="card">
+        <div class="card-start">
+            <img src="/images/${track.albumArt}">
         </div>
-    <div class="card-end">
-        <p class="card-menu">...</p>
-    </div>
-</section>
+            <div class="card-mid">
+                <h4 class="card-title">${track.title}</h4>
+                <p class="card-artist">${track.artist}</p>
+            </div>
+        <div class="card-end">
+            <p class="card-menu">...</p>
+        </div>
+    </section>
     `
 })
 
