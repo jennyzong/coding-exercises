80c331b46652bfeef3a5be79e60f03a6f981a8b4
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-06-21 11:44:03 -0400

    Aside: inline arrow function challenge
    - Refactor the .map method so the inline function is an arrow function.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const speedWarning = (speedLimit, actualSpeed)=> {
-    if( speedLimit < actualSpeed){
-        return `You are going at ${actualSpeed} mph!`
-    }
-}    
-console.log(speedWarning(80, 40))
+const distanceTraveledMiles = [267, 345, 234, 190, 299]
+
+const distanceTraveledKm = distanceTraveledMiles.map( distance => Math.round(distance * 1.6 ))
+
+console.log(distanceTraveledKm)
 
 /*
 Challenge
-1. Refactor this function so it only warns drivers 
-   who are going over the speed limit.
-2. The function now needs to take in two parameters. 
-   The first is the speed limit, the second is the 
-   driver's actual speed.
+1. Refactor this .map method so the inline function is
+   an arrow function. 
+2. Write the least amount of code possible.
 */
