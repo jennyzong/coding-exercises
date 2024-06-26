356538eeeebc129b1d08013f13ed4be8c5f95da7
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-06-26 19:50:40 -0400

    Super Challenge Set-up
    - use- import/export .map(), .join(),  Object destructuring,  .reduce(),  Default parameter to render out a card

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-import { itemsBoughtArr } from '/itemsBoughtArr.js'
+import { propertyForSaleArr } from '/properties/propertyForSaleArr'
+import { placeholderPropertyObj } from '/properties/placeholderPropertyObj'
 
-function calculateTotalCost(itemsBoughtArr){
+function getPropertyHtml(propertyArr = [placeholderPropertyObj]) {
+    return propertyArr.map(property =>{
+        const {
+               propertyLocation,
+               priceGBP,
+               roomsM2,
+               comment,
+               image
+        } = property
+        const total = roomsM2.reduce((total, current) => total + current)
+         return 
+         `<section class="card">
+                <img src="/images/${image}">
+                <div class="card-right">
+                    <h2>${propertyLocation}</h2>
+                    <h3>${priceGBP}</h3>
+                    <p>${comment}</p>
+                    <h3>${total} m&sup2</h3>
+                </div>
+            </section>`
+             
+    })
 /*
-Challenge:
-1. Use the reduce method to calculate the total 
-   cost of items which have been bought.
+SUPER CHALLENGE 💪
+
+Render out a card for each of the properties in the propertyForSaleArr array (in the 'properties' folder). Each card should have an image, a property location, a price, a comment and the TOTAL property size in square metres (each object has an array with the size in square metres of the individual rooms).
+
+If no array of properties is passed to getPropertyHtml, the placeholder property stored in placeholderPropertyObj (in the 'properties' folder) should be rendered instead.
+
+This is the JS I want you to use to complete this challenge 👇
+- import/export
+- .map()
+- .join()
+- Object destructuring
+- .reduce()
+- Default parameters
+
+The HTML and CSS have been done for you. 
+This is the HTML template 👇. Replace everything in UPPERCASE with property data.
+
+<section class="card">
+    <img src="/images/IMAGE">
+    <div class="card-right">
+        <h2>PROPERTY LOCATION</h2>
+        <h3>PRICE GBP</h3>
+        <p>COMMENT</p>
+        <h3>TOTAL SIZE IN SQUARE METRES m&sup2;</h3>
+    </div>
+</section> 
 */
-    const total = 
-    return total
 }
 
-console.log(calculateTotalCost(itemsBoughtArr))
+/***** Modify 👇 by adding an argument to the function call ONLY. *****/
+document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
