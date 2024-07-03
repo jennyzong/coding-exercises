9436ea6100e7cb05df5816dfca0ae584f3e17a26
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-07-02 20:42:00 -0400

    Objects to Constructor Functions

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const gamer = {
-    name: 'Dave',
-    score: 0,
-    incrementScore: function(){
-        this.score++   
+// const gamer = {
+//     name: 'Dave',
+//     score: 0,
+//     incrementScore: function(){
+//         this.score++   
+//     }
+// }
+
+function Gamer(name){
+    this.name = name
+    this.score = 0
+    this.incrementScore = function(){
+        this.score++  
     }
 }
 
-const gamer1 = {
-    name: 'Sarah',
-    score: 0,
-    incrementScore: function(){
-        this.score++   
-    }
-}
+const dave = new Gamer('Dave')
+const sarah = new Gamer('Sarah')
+const kerry = new Gamer('Kerry')
+dave.incrementScore()
+sarah.incrementScore()
+sarah.incrementScore()
+kerry.incrementScore()
+kerry.incrementScore()
+kerry.incrementScore()
+console.log(dave)
+console.log(sarah)
+console.log(kerry)
 
-gamer.incrementScore()
-gamer1.incrementScore()
-console.log(gamer)
-console.log(gamer1)
+
+
+
