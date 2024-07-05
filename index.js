085860ca05a0c9b0e6973b3972a3f6d40d859cda
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-07-05 12:35:07 -0400

    Constructor Function Callenge
    - use constructor function to log out a sentence like `Merlin now has: wand, map, potion`.

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-// const gamer = {
-//     name: 'Dave',
-//     score: 0,
-//     incrementScore: function(){
-//         this.score++   
-//     }
-// }
-
-function Gamer(name){
-    this.name = name
-    this.score = 0
-    this.incrementScore = function(){
-        this.score++  
-    }
-}
-
-const dave = new Gamer('Dave')
-const sarah = new Gamer('Sarah')
-const kerry = new Gamer('Kerry')
-dave.incrementScore()
-sarah.incrementScore()
-sarah.incrementScore()
-kerry.incrementScore()
-kerry.incrementScore()
-kerry.incrementScore()
-console.log(dave)
-console.log(sarah)
-console.log(kerry)
-
-
-
-
+/*
+Challenge:
+1. Create a constructor function called 'Character'.
+2. Give it 'name' and 'collectedItemsArr' properties. 
+    - 'name' should hold the character’s name.
+	- 'collectedItemsArr' should hold an array of items. 
+       Initialise it to an empty array.
+3. Create an 'addItem' method which takes in an item as 
+   a parameter and adds it to 'collectedItemsArr'.
+4. The addItem method should log out a sentence like 
+   `Merlin now has: wand, map, potion`.
+5. Check it’s working by creating several instances of 
+   Character and adding items to their arrays.
+*/
