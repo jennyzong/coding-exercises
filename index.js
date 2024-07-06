8da6b4e37c5878afd1959daf6c77d6a0a3f5a69b
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-07-06 19:38:08 -0400

    Debugging: Errors

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-/*
-Challenge:
-Rebuild this constructor function as a class.
-*/
+const ageInput = document.getElementById('age')
+const resultDisplay = document.getElementById('result-display')
+const checkButton = document.getElementById('btn-check')
 
+//set minimum drinking and driving ages for a jurisdiction 
+const minDrinkAge = 16
+const minDriveAge = 18
 
-class Character {
-    constructor(name) {
-        this.name = name
-        this.collectedItemsArr = []
-    }
-    
-    addItem(item){
-        this.collectedItemsArr.push(item)
-        console.log(`${this.name} now has: ${this.collectedItemsArr.join(', ')}`)    
-    }
+//check button click event listener
+checkButton.addEventListener('click', function () {
+    let message = ''
+    const age = ageInput.value
+        // Age is below the minDrinkAge and minDriveAge
+        if (age < minDrinkAge && age < minDriveAge) {
+            message = `I'm sorry, you cannot drink or drive ⛔`
+        // Relevant to countries where minDrinkAge is less than minDriveAge
+        } else if (age >= minDrinkAge && age < minDriveAge) {
+            message = "You can drink 🍺 but you cannot drive"
+        // Relevant to countries where minDriveAge is less than minDrinkAge
+        } else if (age >= minDriveAge && age < minDrinkAge) {
+            message = "You can drive 🚗 but you cannot drink"
+        // Age is above the minDrinkAge and minDriveAge
+        } else {
+            message = "You can drink 🍺 and drive 🚗 (not at the same time though!)"
+        } 
+        renderMessage(message)
+})
+
+function renderMessage(message) {
+    resultDisplay.innerText = message
 }
 
-const wizard = new Character('Merlin')
-wizard.addItem('a wand')
+
